dd1f30a91aee1e4a573bc4ef1c897c8fa035666a
Denis Efimov <defimov9@gmail.com>
2023-02-06 23:50:21 +0500

    feature/add-initial-cards

--- scripts/index.js
+++ scripts/index.js
@@ -6,26 +6,51 @@ const currJob = document.querySelector('.profile__job');
 const popupForm = document.querySelector('.popup__form');
 const nameField = popupForm.querySelector('.popup__input_type_name');
 const jobField = popupForm.querySelector('.popup__input_type_job');
+const cardsContainer = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template').content;
 
 const closeOrOpenPopup = () => {
-    popup.classList.toggle('popup_opened');
-    if (popup.classList.contains('popup_opened')) {
-        handleCurrentProfileInfo();
-    }
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    handleCurrentProfileInfo();
+  }
 };
 
 const editProfileData = (event) => {
-    event.preventDefault();
-    currName.textContent = nameField.value;
-    currJob.textContent = jobField.value;
-    closeOrOpenPopup();
+  event.preventDefault();
+  currName.textContent = nameField.value;
+  currJob.textContent = jobField.value;
+  closeOrOpenPopup();
 };
 
 const handleCurrentProfileInfo = () => {
-    nameField.value = currName.textContent;
-    jobField.value = currJob.textContent;
+  nameField.value = currName.textContent;
+  jobField.value = currJob.textContent;
 };
 
 editButton.addEventListener('click', closeOrOpenPopup);
 closeButton.addEventListener('click', closeOrOpenPopup);
 popupForm.addEventListener('submit', editProfileData);
+
+const createCardElement = ({ name, link }) => {
+  const cardElement = cardTemplate
+    .querySelector('.elements__card')
+    .cloneNode(true);
+  const cardTitle = cardElement.querySelector('.elements__title');
+  const cardImage = cardElement.querySelector('.elements__image');
+
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  return cardElement;
+};
+
+const renderInitialCards = (cards) => {
+  cards.forEach((card) => {
+    const cardElement = createCardElement(card);
+    cardsContainer.append(cardElement);
+  });
+};
+
+renderInitialCards(initialCards);
