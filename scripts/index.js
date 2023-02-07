c7d6bde27d13d75990051a06de79c6715fa20c14
Denis Efimov <defimov9@gmail.com>
2023-02-07 18:15:16 +0500

    feature/create-popup-for-adding-photos-and-add-logic-for-him-and-some-more

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,26 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close');
-const popup = document.querySelector('.popup');
+const addButton = document.querySelector('.profile__add-button');
+const editProfilePopup = document.querySelector('.popup_type_edit-profile');
+const addPhotoPopup = document.querySelector('.popup_type_add-photo');
 const currName = document.querySelector('.profile__name');
 const currJob = document.querySelector('.profile__job');
-const popupForm = document.querySelector('.popup__form');
-const nameField = popupForm.querySelector('.popup__input_type_name');
-const jobField = popupForm.querySelector('.popup__input_type_job');
+const nameField = document.querySelector('.popup__input_type_name');
+const jobField = document.querySelector('.popup__input_type_job');
 const cardsContainer = document.querySelector('.elements');
 const cardTemplate = document.querySelector('#card-template').content;
-
-const closeOrOpenPopup = () => {
-  popup.classList.toggle('popup_opened');
-  if (popup.classList.contains('popup_opened')) {
-    handleCurrentProfileInfo();
-  }
-};
-
-const editProfileData = (event) => {
-  event.preventDefault();
-  currName.textContent = nameField.value;
-  currJob.textContent = jobField.value;
-  closeOrOpenPopup();
-};
+let currPopup, currForm;
 
 const handleCurrentProfileInfo = () => {
   nameField.value = currName.textContent;
   jobField.value = currJob.textContent;
 };
 
-editButton.addEventListener('click', closeOrOpenPopup);
-closeButton.addEventListener('click', closeOrOpenPopup);
-popupForm.addEventListener('submit', editProfileData);
+const editProfileData = (event) => {
+  event.preventDefault();
+  currName.textContent = nameField.value;
+  currJob.textContent = jobField.value;
+  closePopup();
+};
 
 const createCardElement = ({ name, link }) => {
   const cardElement = cardTemplate
@@ -38,13 +28,58 @@ const createCardElement = ({ name, link }) => {
     .cloneNode(true);
   const cardTitle = cardElement.querySelector('.elements__title');
   const cardImage = cardElement.querySelector('.elements__image');
+  const cardLike = cardElement.querySelector('.elements__like');
+  const cardRemove = cardElement.querySelector('.elements__remove');
 
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
 
+  cardLike.addEventListener('click', () => {
+    cardLike.classList.toggle('elements__like_active');
+  });
+
+  cardRemove.addEventListener('click', () => {
+    cardElement.remove();
+  });
+
   return cardElement;
 };
+
+const addCard = (event) => {
+  event.preventDefault();
+  const name = currPopup.querySelector('.popup__input_type_title').value;
+  const link = currPopup.querySelector('.popup__input_type_url').value;
+  if (!name && !link) return;
+  cardsContainer.prepend(createCardElement({ name, link }));
+  closePopup();
+};
+
+const closePopup = () => {
+  currForm.reset();
+  currPopup.classList.remove('popup_opened');
+};
+
+const openPopup = (event) => {
+  currPopup = event.target;
+  if (currPopup.classList.contains('profile__edit-button')) {
+    currPopup = editProfilePopup;
+    handleCurrentProfileInfo();
+    currForm = currPopup.querySelector('.popup__form');
+    currForm.addEventListener('submit', editProfileData);
+  }
+  if (currPopup.classList.contains('profile__add-button')) {
+    currPopup = addPhotoPopup;
+    currForm = currPopup.querySelector('.popup__form');
+    currForm.addEventListener('submit', addCard);
+  }
+  currPopup.classList.add('popup_opened');
+  const closeButton = currPopup.querySelector('.popup__close');
+  closeButton.addEventListener('click', closePopup);
+};
+
+editButton.addEventListener('click', openPopup);
+addButton.addEventListener('click', openPopup);
 
 const renderInitialCards = (cards) => {
   cards.forEach((card) => {
