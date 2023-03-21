d1bea39d558f006faf2891b17db373b541017de7
Denis Efimov <defimov9@gmail.com>
2023-03-21 13:26:34 +0500

    hotfixes/ refactoring by tests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,33 +33,6 @@ const validationConfig = {
   errorClass: 'popup__input-error_active',
 };
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
 export {
   editButton,
   addButton,
@@ -68,7 +41,6 @@ export {
   cardTemplateSelector,
   apiConfig,
   validationConfig,
-  initialCards,
   cardsContainerSelector,
   photoPopupSelector,
   userJobSelector,
